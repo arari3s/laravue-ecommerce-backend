8bee9d8102c4e3afdba149ded2a5b5c6055e68ad
arari3s <arlanariandi@gmail.com>
2023-04-04 04:33:51 +0700

    UPDATE: Update model productGallery

--- app/Models/ProductGallery.php
+++ app/Models/ProductGallery.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductGallery extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'products_id', 'photo', 'is_default'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'products_id', 'id');
+    }
+
+    public function getPhotoAttribute($value)
+    {
+        return url('storage/' . $value);
+    }
 }
