f61e8dbd71bc6cf5592f0279e4685cc19707ce4e
arari3s <arlanariandi@gmail.com>
2023-04-04 22:32:31 +0700

    UPDATE: Update model transaction and transaction detail add relation in transaction and transaction detail

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'uuid', 'name', 'email', 'number', 'address', 'transaction_total', 'transaction_status'
+    ];
+
+    public function details()
+    {
+        return $this->hasMany(TransactionDetail::class, 'transactions_id');
+    }
 }
--- app/Models/TransactionDetail.php
+++ app/Models/TransactionDetail.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TransactionDetail extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'products_id', 'transactions_id'
+    ];
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class, 'transactions_id', 'id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'products_id', 'id');
+    }
 }
