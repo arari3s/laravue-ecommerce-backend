b7a972991331056fb156cdd925aec9fbcbe8aa5d
arari3s <arlanariandi@gmail.com>
2023-04-04 15:17:52 +0700

    UPDATE: Update request ProductGalleryRequest

---
+++ app/Http/Requests/ProductGalleryRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class ProductGalleryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'products_id' => 'required|integer|exists:products,id',
+            'photo' => 'required|image',
+            'is_default' => 'boolean'
+        ];
+    }
+}
